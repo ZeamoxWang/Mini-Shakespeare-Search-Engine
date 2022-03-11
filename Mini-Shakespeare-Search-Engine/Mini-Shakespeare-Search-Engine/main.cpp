01d42d5b188cf8e1a834a89ec647f1c77f061eda
ZeamoxWang <wangzimo@zju.edu.cn>
2022-03-11 23:09:04 +0800

    WZM done！

--- Mini-Shakespeare-Search-Engine/Mini-Shakespeare-Search-Engine/main.cpp
+++ Mini-Shakespeare-Search-Engine/Mini-Shakespeare-Search-Engine/main.cpp
@@ -25,11 +25,12 @@ int main(void)
 		/*
 		* 待完成的工作还有：
 		* 0.创建多个字典对象，创建多个文件夹，象征性地完成大文件管理的工作；
-		*	我已经创建了Dics对象了（见dictionary.h），就用这个Dics管理下属的一串的dictionary[]我感觉就ok，
+		*	我已经创建了Dics对象了（见dictionary.h），就用这个Dics管理下属的一串的dictionary[]我感觉就行，
 		*	然后下面这些操作都对Dics去查，查出来的结果汇总一下；
 		* 1.根据句子，筛选某个rate下取and的结果；
 		* 2.识别减号，把减号前后的结果求差集；
-		* 3.在singleWordQuery中设计模糊查询；（搞不出来就算了）
+		* 3.在singleWordQuery中设计模糊查询；（麻烦就算了）
+		* 4.设计测试样例
 		*/
 	}
 
